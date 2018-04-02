79355a1078470678238409ec0f586825da1d36e2
Yukichi Kawada <yukichikawada@gmail.com>
2018-04-01 20:33:43 -0700

    Add react frontend

---
+++ frontend/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>hi, mom and dad!</h1>, root);
+});
