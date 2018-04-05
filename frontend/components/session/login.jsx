b9a39b7b318d9ed86dbba92c75fdf24e7bc94e0f
Yukichi Kawada <yukichikawada@gmail.com>
2018-04-05 15:03:07 -0700

    Invoke closing modal onSubmit and stop propagation of closing

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -14,7 +14,7 @@ class Login extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     this.props.login(this.state)
-      .then(() => this.props.closeModal);
+      .then(() => this.props.closeModal());
   }
 
   update(field) {
