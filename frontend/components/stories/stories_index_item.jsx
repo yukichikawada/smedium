06ad808b1843d295d564b2186367008fe8740830
Yukichi Kawada <yukichikawada@gmail.com>
2018-04-08 23:59:42 -0700

    Add basic styling to indexItems

--- frontend/components/stories/stories_index_item.jsx
+++ frontend/components/stories/stories_index_item.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const StoriesIndexItem = ({ story }) => (
-  <li>
+  <li className="stories-index-item">
     <Link to={`/stories/${story.id}`}>
-      <h4>{story.title}</h4>
-      <h5>{story.author}</h5>
+      <h4 className="story-item-title">{story.title}</h4>
+      <h5 className="story-item-author">{story.author}</h5>
 
-      <p>{new Date(story.created_at).toDateString().slice(4, 10)} ~ {story.read_time} min read</p>
+      <p className="story-item-stats">{new Date(story.created_at).toDateString().slice(4, 10)} ~ {story.read_time} min read</p>
     </Link>
   </li>
 );
