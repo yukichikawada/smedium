9077f1b83e6bbd8802b72b1e6dd919d35fe23be4
Yukichi Kawada <yukichikawada@gmail.com>
2018-05-18 13:49:25 -0700

    Add reactstrap module

--- frontend/components/stories/stories_index.jsx
+++ frontend/components/stories/stories_index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import CreateStoryContainer from './create_story_form_container';
 import StoriesIndexItem from './stories_index_item';
 
+import { Button } from 'reactstrap';
+
 class StoriesIndex extends React.Component {
   componentDidMount() {
     this.props.fetchStories();
@@ -17,6 +19,7 @@ class StoriesIndex extends React.Component {
 
     return (
       <div className="container">
+        <Button outline color="primary">Submit</Button>
         <ul className="container-index-item">
           {stories}
         </ul>
