31fc9524b36f23ce91c0b93ecbb2a43286ff701c
Yukichi Kawada <yukichikawada@gmail.com>
2018-05-20 23:55:25 -0700

    Move dropdown menu to right, use button for #upgrade. Need to figure out how to flex navbar contents

--- frontend/components/navbar/my_navbar.jsx
+++ frontend/components/navbar/my_navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import {
+  Button,
   Navbar,
   NavbarBrand,
   Nav,
@@ -16,13 +17,41 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 class MyNavbar extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   fixed: 'top'
-    // }
   }
+
   session() {
     let user = this.props.currentUser ? (
-      <div></div>
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <NavLink href="/"><FontAwesomeIcon icon="search"/></NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/"><FontAwesomeIcon icon="bell"/></NavLink>
+        </NavItem>
+        <UncontrolledDropdown nav inNavbar>
+          <DropdownToggle nav>
+            <FontAwesomeIcon icon="user"/>
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem disabled>Become a member</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem><Link to="/newStory">New Story</Link></DropdownItem>
+            <DropdownItem disabled>Your Stories</DropdownItem>
+            <DropdownItem disabled>Stats</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem disabled>Smedium Partner Program</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem disabled>Bookmarks</DropdownItem>
+            <DropdownItem disabled>Publications</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem disabled>Profile</DropdownItem>
+            <DropdownItem disabled>Settings</DropdownItem>
+            <DropdownItem disabled>Help</DropdownItem>
+            <DropdownItem disabled>Help</DropdownItem>
+            <DropdownItem onClick={this.props.logout}>Sign out</DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
     ) : (
       <div className="login-signup-buttons">
         <Link to="/login" className="sign-in-button link-new-story button-link">Sign in</Link>
@@ -35,7 +64,7 @@ class MyNavbar extends React.Component {
 
   upgrade() {
     let plan = this.props.currentUser ? (
-      <NavLink href="/">Upgrade</NavLink>
+      <Button outline color="secondary" size="sm">Upgrade</Button>
     ) : (
       <NavLink href="/">About membership</NavLink>
     )
@@ -59,33 +88,7 @@ class MyNavbar extends React.Component {
             </NavItem>
           </Nav>
           <NavbarBrand href="/">Smedium</NavbarBrand>
-          <Nav className="ml-auto" navbar>
-            <NavItem><FontAwesomeIcon icon="search"/></NavItem>
-            <NavItem><FontAwesomeIcon icon="bell"/></NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav>
-                <FontAwesomeIcon icon="user"/>
-              </DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem disabled>Become a member</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem><Link to="/newStory">New Story</Link></DropdownItem>
-                <DropdownItem disabled>Your Stories</DropdownItem>
-                <DropdownItem disabled>Stats</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem disabled>Smedium Partner Program</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem disabled>Bookmarks</DropdownItem>
-                <DropdownItem disabled>Publications</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem disabled>Profile</DropdownItem>
-                <DropdownItem disabled>Settings</DropdownItem>
-                <DropdownItem disabled>Help</DropdownItem>
-                <DropdownItem disabled>Help</DropdownItem>
-                <DropdownItem onClick={this.props.logout}>Sign out</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
+          {this.session()}
         </Navbar>
       </div>
     );
